d2ea875fba1afae0d577a986a088b90c08782178
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-21 20:50:26 +0200

    Fixata richiesta dopo aver fatto scomparire i numeri

--- JS/script.js
+++ JS/script.js
@@ -16,15 +16,17 @@ const guessNumb = []
 const timer = setInterval(function(){
     console.log(time);
     if(time <= 0){
+        articleElement.classList.add('d-none');
         clearInterval(timer);
         console.log('Finito il tempo!');
-        articleElement.classList.add('d-none');
-        for(let i = 0; i < 5; i++){
-            const guess = Number.parseInt(prompt('Inserisci i numeri che ti ricordi!'))
-            guessNumb.push(guess);
-        }
-        console.log(guessNumb)
     }
     time = time - 1;
 },1000);
+const timeToGuess = setTimeout(function(){
+    for(let i = 0; i < 5; i++){
+        const guess = Number.parseInt(prompt('Inserisci i numeri che ti ricordi!'))
+        guessNumb.push(guess);
+    }
+    console.log(guessNumb);
+},4500);
 console.log(randomFive);
