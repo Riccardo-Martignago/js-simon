def973cf426f928b0d577d142f2ea6e48b28f651
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-21 18:39:42 +0200

    Creata funzione per generare numeri random, creato ciclo per stampare i numeri in pagina e aggiungerli nell'array

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-console.log('Hello!')
+function genRandomNum (max, min){
+    return Math.floor(Math.random()* ((max + 1) - min)) + min;
+}
+const sectionEl = document.querySelector('section.grid');
+const randomFive = [];
+for(let i = 0; i < 5; i++){
+    const numb = genRandomNum (1000, 1);
+    randomFive.push(numb);
+    const artEl = document.createElement('article');
+    sectionEl.appendChild(artEl);
+    artEl.append(numb);
+}
+console.log(randomFive)
